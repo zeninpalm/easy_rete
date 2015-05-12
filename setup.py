84e7b09d26aecf103e4e9f432bbc796a08d56583
zeninpalm <yiwei.in.cyber@gmail.com>
2015-05-12 16:18:21 +0800

    Passes trivial tests

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Simple and clean RETE algorithm implentation',
+    'author': 'Yi Wei',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'My email.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'EasyRete'
+}
+
+setup(**config)
---
+++ tests/integration/rete_tests.py
@@ -0,0 +1,11 @@
+import unittest
+from easy_rete.network import Network
+
+class ReteTests(unittest.TestCase):
+
+    def test_creation(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
+
